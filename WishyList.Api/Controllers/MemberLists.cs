1aaa72bf020344f625d3511b2ea2c81db70c479b
kuark7 <Crossley-Dustin@users.noreply.github.com>
2021-06-01 00:58:59 -0700

    Add MemberLists controller.

---
+++ WishyList.Api/Controllers/MemberLists.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WishyList.Api.Models;
+using WishyList.Models;
+
+namespace WishyList.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MemberLists : ControllerBase
+    {
+        private readonly IListRepository listRepository;
+
+        public MemberLists(IListRepository listRepository)
+        {
+            this.listRepository = listRepository;
+        }
+
+        [HttpGet("{id:int }")]
+        public async Task<ActionResult<List>> GetMemberLists(int memberId)
+        {
+            try
+            {
+                return Ok( await listRepository.GetMemberLists(memberId));
+
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from the database");
+            }
+        }
+    }
+}
